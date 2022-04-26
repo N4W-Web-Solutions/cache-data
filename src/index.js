a9aba241ca64a791f1a7248fcc16a314b61ee36b
Piero Ribeiro <pieroribeiro@sbt.com.br>
2022-04-26 14:44:46 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+class Cache {
+    constructor () {
+        this.records = []
+        this.minutesToExpires = 5
+    }
+    model (obj) {
+        return {
+            key: obj.key,
+            value: obj.value,
+            expires: new Date(new Date().getTime() + (60000 * this.minutesToExpires)).getTime()
+        }
+    }
+    exists (key) {
+        const record = this.records.find(record => record.key === key)
+        return (record) ? true : false
+    }
+    isExpired (key) {
+        const record = this.records.find(record => record.key === key)
+        return (record) ? ((record.expires < new Date().getTime()) ? true : false) : false
+    }
+    get (key) {
+        return this.records.find(record => record.key === key)
+    }
+    insert (key, value) {
+        this.records.push(this.model({key, value}))
+    }
+    remove (key) {
+        const i = this.records.findIndex(v => v.key === key)
+        this.records.splice(i, i >= 0 ? 1 : 0)
+    }
+}
+
+exports = Cache
